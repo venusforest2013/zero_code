571c4133a70c86ffed026dbfdf2eeb9c983bfda8
dongbaosen <dongbaosen@bytedance.com>
2021-12-24 11:11:04 +0800

    add stack
    Change-Id: I5e1af86d0513c234a4ae294439427c5d7ba15278

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/venusforest2013/zero_code/stack"
-)
+import "fmt"
 
 func main() {
+	arr := []int{6, 5, 4, 3}
+	heapSort(arr)
+	fmt.Println(arr)
+}
 
-	s := &stack.LinkStack{}
-	s.Push(1)
-	s.Push(2)
-	s.PrintStack()
-	ret := s.Pop()
-	fmt.Println(ret)
-	fmt.Println("====")
-	s.PrintStack()
+func heapify(arr []int, pos, count int) {
+	for {
+		child := pos * 2
+		if child > count {
+			break
+		}
+		if child < count && arr[child+1] > arr[child] {
+			child = child + 1
+		}
+		if arr[pos] > arr[child] {
+			break
+		}
+		arr[pos], arr[child] = arr[child], arr[pos]
+		pos = child
+	}
+
+}
+
+func heapSort(arr []int) {
+	count := len(arr) - 1
+
+	if count <= 1 {
+		return
+	}
+
+	for i := count / 2; i >= 1; i-- {
+		heapify(arr, i, count)
+	}
+
+	for count >= 1 {
+		arr[1], arr[count] = arr[count], arr[1]
+		count--
+		heapify(arr, 1, count)
+	}
 }
